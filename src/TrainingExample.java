8a33933303c04fd97af2fedd5305ad7fbbdf7359
jleveille623 <jgleveille@gmail.com>
2019-10-17 11:09:32 -0700

    insert message here

---
+++ src/TrainingExample.java
@@ -0,0 +1,25 @@
+public class TrainingExample {
+    private String text;
+    private boolean isPredatory;
+
+    public TrainingExample(String text, boolean isPredatory) {
+        this.text = text;
+        this.isPredatory = isPredatory;
+    }
+
+    public void setPredatory(boolean predatory) {
+        isPredatory = predatory;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean getIsPredatory() {
+        return isPredatory;
+    }
+
+    public String getText() {
+        return text;
+    }
+}

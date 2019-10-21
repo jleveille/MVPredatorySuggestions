caa0bd0ff47d2da40eb7f9014117d13a101e0a0d
jleveille623 <jgleveille@gmail.com>
2019-10-21 09:14:55 -0700

    Add TextLib class

---
+++ src/TextLib.java
@@ -0,0 +1,90 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TextLib {
+    public static String readFileAsString(String filename) {
+        Scanner scanner;
+        StringBuilder output = new StringBuilder();
+
+        try {
+            scanner = new Scanner(new FileInputStream(filename), "UTF-8");
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                output.append(line.trim()+"\n");
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found " + filename);
+        }
+
+        return output.toString();
+    }
+
+    public static ArrayList<TrainingExample> readDocInfo(String filename){
+        ArrayList<TrainingExample> trainingExampleList = new ArrayList<>();
+        Scanner scanner;
+
+        try {
+            scanner = new Scanner(new FileInputStream(filename), "UTF-8");
+            String line = scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                TrainingExample example = processLine(line);
+                trainingExampleList.add(example);
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found " + filename);
+        }
+        return trainingExampleList;
+    }
+
+    private static DocInfo processLine(String line) {
+        String[] values = line.split(",");
+        String filename = values[0].trim();
+        double TRAD_FLESCH = Double.parseDouble(values[91].trim());
+        double TRAD_KINCAID = Double.parseDouble(values[92].trim());
+
+        return new DocInfo(filename, TRAD_FLESCH, TRAD_KINCAID);
+    }
+
+    public static ArrayList<String> splitIntoSentences(String text) {
+        ArrayList<String> output = new ArrayList<>();
+
+        Locale locale = Locale.US;
+        BreakIterator breakIterator = BreakIterator.getSentenceInstance(locale);
+        breakIterator.setText(text);
+
+        int prevIndex = 0;
+        int boundaryIndex = breakIterator.first();
+        while(boundaryIndex != BreakIterator.DONE) {
+            String sentence = text.substring(prevIndex, boundaryIndex).trim();
+            if (sentence.length()>0)
+                output.add(sentence);
+            prevIndex = boundaryIndex;
+            boundaryIndex = breakIterator.next();
+        }
+
+        String sentence = text.substring(prevIndex).trim();
+        if (sentence.length()>0)
+            output.add(sentence);
+
+        return output;
+    }
+
+    private static String getWordFromLine(String line) {
+        return line.substring(0, line.indexOf("="));
+    }
+
+
+}
+

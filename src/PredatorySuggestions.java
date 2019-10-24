8c930fb6a4c5e2d172ae572bac087fe35bbe9dd2
jleveille623 <jgleveille@gmail.com>
2019-10-24 10:05:19 -0700

    Fix merge problems from before

--- src/PredatorySuggestions.java
+++ src/PredatorySuggestions.java
@@ -2,16 +2,24 @@ import java.util.ArrayList;
 
 public class PredatorySuggestions {
     public static final ArrayList<TrainingExample> PRONOUNS_LIST = TextLib.readPronouns("data/Pronouns");
-
-
     private static final ArrayList<TrainingExample> PREDATORY_PHRASES_LIST = TextLib.readPredatoryPhrases("data/GuiltAndThreat.txt");
+
+    public static boolean isPredatory(String text) {
+        if (containsPredatoryPhrases(text)) return true;
+        if (containsPronouns(text)) return true;
+
+        return false;
+    }
 
     private static boolean containsPredatoryPhrases(String text) {
         int predatoryCount = 0, notpredatoryCount = 0;
         for(TrainingExample example: PREDATORY_PHRASES_LIST) {
             if (text.contains(example.getText())){
-                if(example.getIsPredatory() == true) predatoryCount++;
-                if (example.getIsPredatory() == false) notpredatoryCount++;
+                if(example.getIsPredatory() == true) {
+                    predatoryCount++;
+                } else {
+                    notpredatoryCount++;
+                }
             }
         }
         if (predatoryCount > notpredatoryCount) {
@@ -21,15 +29,7 @@ public class PredatorySuggestions {
         }
     }
 
-
-    public boolean isPredatory(String text) {
-        if (containsPredatoryPhrases(text)) return true;
-        if (containsPronouns(text)) return true;
-
-        return false;
-    }
-
-    private boolean containsPronouns(String text) {
+    private static boolean containsPronouns(String text) {
         String[] words = text.split(" ");
         for (TrainingExample pronoun : PRONOUNS_LIST) {
             for (String word : words) {

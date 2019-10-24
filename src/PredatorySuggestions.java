df7134982bca744be1822aaaa117d1cf9572cbb7
jleveille623 <jgleveille@gmail.com>
2019-10-24 10:53:22 -0700

    Made it reaady to run first time

--- src/PredatorySuggestions.java
+++ src/PredatorySuggestions.java
@@ -2,15 +2,22 @@ import java.util.ArrayList;
 
 public class PredatorySuggestions {
     public static final ArrayList<String> PRONOUNS_LIST = TextLib.readOneWordFiles("data/Pronouns");
-    private static final ArrayList<TrainingExample> PREDATORY_PHRASES_LIST = TextLib.readPredatoryPhrases("data/GuiltAndThreat.txt");
-
-    //put our methods here
+    public static final ArrayList<String> ACTIONS_LIST = TextLib.readOneWordFiles("data/Action.txt");
+    private static final ArrayList<TrainingExample> PREDATORY_PHRASES_LIST = TextLib.readPredatoryPhrases("data/GuiltAndThreat(LargeText).txt");
 
     public static boolean isPredatory(String text) {
-        if (containsPredatoryPhrases(text)) return true;
-        if (containsPronouns(text)) return true;
-
-        return false;
+        int predatoryCount = 0, notpredatoryCount = 0;
+        if (pronounFollowedByAction(text)) {
+            predatoryCount++;
+        } else {
+            notpredatoryCount++;
+        }
+        if (containsPredatoryPhrases(text)) {
+            predatoryCount++;
+        } else {
+            notpredatoryCount++;
+        }
+        return (predatoryCount > notpredatoryCount);
     }
 
     private static boolean containsPredatoryPhrases(String text) {
@@ -27,19 +34,33 @@ public class PredatorySuggestions {
         if (predatoryCount > notpredatoryCount) {
             return true;
         } else {
-            return false;
+            return false; //not enough information
         }
     }
 
-    private static boolean containsPronouns(String text) {
+    private static boolean pronounFollowedByAction(String text){
         String[] words = text.split(" ");
+        for (int i = 0; i < words.length -1; i++) {
+            if(containsPronouns(words[i]) && containsActions(words[i+1])) return true;
+        }
+        return false;
+    }
+
+    private static boolean containsPronouns(String word) {
         for (String pronoun : PRONOUNS_LIST) {
-            for (String word : words) {
                 if (word.equals(pronoun)) {
                     return true;
                 }
             }
-        }
+        return false;
+    }
+
+    private static boolean containsActions(String word) {
+        for (String action : ACTIONS_LIST) {
+                if (word.equals(action)) {
+                    return true;
+                }
+            }
         return false;
     }
 }

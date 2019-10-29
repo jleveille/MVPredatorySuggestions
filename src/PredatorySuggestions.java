6d734a9145a698ead88bbd072b60668b9d3f94ea
jleveille623 <jgleveille@gmail.com>
2019-10-29 10:34:49 -0700

    Update pronoun followed by action method to test pos or neg of action

--- src/PredatorySuggestions.java
+++ src/PredatorySuggestions.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 
 public class PredatorySuggestions {
     private static final ArrayList<String> PRONOUNS_LIST = TextLib.readOneWordFiles("data/Names.txt");
-    private static final ArrayList<String> ACTIONS_LIST = TextLib.readOneWordFiles("data/Action.txt");
+    private static final ArrayList<TrainingExample> ACTIONS_LIST = TextLib.readTwoWordFiles("data/Action.txt");
     private static final ArrayList<TrainingExample> PREDATORY_PHRASES_LIST = TextLib.readPredatoryPhrases("data/GuiltAndThreat(LargeText).txt");
 
     public static boolean isPredatory(String text) {
         int predatoryCount = 0, notpredatoryCount = 0;
-        if (pronounFollowedByAction(text)) {
+        if (pronounFollowedByNegAction(text)) {
             predatoryCount++;
         } else {
             notpredatoryCount++;
@@ -22,9 +22,9 @@ public class PredatorySuggestions {
 
     private static boolean containsPredatoryPhrases(String text) {
         int predatoryCount = 0, notpredatoryCount = 0;
-        for(TrainingExample example: PREDATORY_PHRASES_LIST) {
-            if (text.contains(example.getText())){
-                if(example.getIsPredatory() == true) {
+        for (TrainingExample example : PREDATORY_PHRASES_LIST) {
+            if (text.contains(example.getText())) {
+                if (example.getIsPredatory() == true) {
                     predatoryCount++;
                 } else {
                     notpredatoryCount++;
@@ -38,20 +38,45 @@ public class PredatorySuggestions {
         }
     }
 
-    private static boolean pronounFollowedByAction(String text){
-        String[] words = text.split(" ");
-        for (int i = 0; i < words.length -1; i++) {
-            if(containsList(words[i], PRONOUNS_LIST) && containsList(words[i+1], ACTIONS_LIST)) return true;
+    private static boolean pronounFollowedByNegAction(String text) {
+        ArrayList<String> words = breakIntoWords(text);
+        for (int i = 0; i < words.size() - 1; i++) {
+            if (containsList(words.get(i), PRONOUNS_LIST)) {
+                for (TrainingExample example : ACTIONS_LIST) {
+                    if (words.get(i + 1).equals(example))
+                        if (example.getIsPredatory()) return true;
+                }
+            }
         }
         return false;
     }
 
     private static boolean containsList(String word, ArrayList<String> list) {
         for (String item : list) {
-                if (word.equals(item)) {
-                    return true;
-                }
+            if (word.equals(item)) {
+                return true;
             }
+        }
         return false;
+    }
+
+    private static String removePunctuation(String input) {
+        String word = input.toLowerCase();
+        String newWord = "";
+        for (int i = 0; i < word.length(); i++) {
+            String letter = word.substring(i, i + 1);
+            if ("abcdefghijklmnopqrstuvwxyz1234567890".contains(letter)) newWord += letter;
+        }
+        return newWord;
+    }
+
+    private static ArrayList<String> breakIntoWords(String sentence) {
+        ArrayList<String> words = new ArrayList<>();
+        String[] wordArr = sentence.split(" ");
+        for (String word : wordArr) {
+            word = removePunctuation(word);
+            if (!word.equals("")) words.add(word);
+        }
+        return words;
     }
 }

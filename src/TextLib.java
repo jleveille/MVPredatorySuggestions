81f7e75456228c287c7c6cc07a7639e60a557772
jleveille623 <jgleveille@gmail.com>
2019-10-29 11:11:13 -0700

    Added readDataSet method

--- src/TextLib.java
+++ src/TextLib.java
@@ -15,7 +15,7 @@ public class TextLib {
             scanner = new Scanner(new FileInputStream(filename), "UTF-8");
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                output.append(line.trim()+"\n");
+                output.append(line.trim() + "\n");
             }
 
             scanner.close();
@@ -27,7 +27,27 @@ public class TextLib {
         return output.toString();
     }
 
-    public static ArrayList<TrainingExample> readTwoWordFiles(String filename){
+    public static ArrayList<TrainingExample> readDataSet(String filename) {
+        ArrayList<TrainingExample> trainingExampleList = new ArrayList<>();
+        Scanner scanner;
+
+        try {
+            scanner = new Scanner(new FileInputStream(filename), "UTF-8");
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                TrainingExample example = processLineDataSet(line);
+                trainingExampleList.add(example);
+            }
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found " + filename);
+        }
+        return trainingExampleList;
+    }
+
+    public static ArrayList<TrainingExample> readTwoWordFiles(String filename) {
         ArrayList<TrainingExample> trainingExampleList = new ArrayList<>();
         Scanner scanner;
 
@@ -47,7 +67,7 @@ public class TextLib {
         return trainingExampleList;
     }
 
-    public static ArrayList<TrainingExample> readPredatoryPhrases(String filename){
+    public static ArrayList<TrainingExample> readPredatoryPhrases(String filename) {
         ArrayList<TrainingExample> predatoryPhrasesList = new ArrayList<>();
         Scanner scanner;
 
@@ -67,7 +87,7 @@ public class TextLib {
         return predatoryPhrasesList;
     }
 
-    public static ArrayList<String> readOneWordFiles(String filename){
+    public static ArrayList<String> readOneWordFiles(String filename) {
         ArrayList<String> list = new ArrayList<>();
         Scanner scanner;
 
@@ -87,6 +107,21 @@ public class TextLib {
         return list;
     }
 
+    private static TrainingExample processLineDataSet(String line) {
+        boolean isPredatory = false;
+        String text = "";
+        int i = line.length() -1;
+        String newLine = line.substring(i);
+        while (!newLine.contains(",")) {
+            i--;
+            newLine = line.substring(i);
+        }
+            isPredatory = Boolean.parseBoolean(newLine.substring(newLine.indexOf(" ")).trim());
+            text = line.substring(0, i).trim();
+
+        return new TrainingExample(text, isPredatory);
+    }
+
     private static TrainingExample processLineTwoWordFiles(String line) {
         String[] values = line.split(",");
         String text = values[0].trim();
@@ -102,13 +137,33 @@ public class TextLib {
         boolean isPredatory;
         if (connotation.equals("pos")) {
             isPredatory = false;
-        } else if (connotation.equals("neg")){
+        } else if (connotation.equals("neg")) {
             isPredatory = true;
         } else {
             System.out.println("THERE IS AN ERROR IN PROCESS LINE FOR PHRASE");
             isPredatory = true;
         }
         return new TrainingExample(phrase, isPredatory);
+    }
+
+    private static String removePunctuation(String input) {
+        String word = input.toLowerCase();
+        String newWord = "";
+        for (int i = 0; i < word.length(); i++) {
+            String letter = word.substring(i, i + 1);
+            if ("abcdefghijklmnopqrstuvwxyz1234567890 ".contains(letter)) newWord += letter;
+        }
+        return newWord;
+    }
+
+    private static ArrayList<String> breakIntoWords(String sentence) {
+        ArrayList<String> words = new ArrayList<>();
+        String[] wordArr = sentence.split(" ");
+        for (String word : wordArr) {
+            word = removePunctuation(word);
+            if (!word.equals("")) words.add(word);
+        }
+        return words;
     }
 
 }
